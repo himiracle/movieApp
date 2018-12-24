f099a3a7e34c4e07cacf5be798527405f653a2bc
blockchain1124 <blockchain1124@gmail.com>
2018-12-24 18:24:19 +0900

    Updates

---
+++ precache-manifest.957f327785fdf64a811b4901ffd272ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6386f9f7a7cb40ac0a5",
+    "url": "/movieApp/static/js/runtime~main.c6386f9f.js"
+  },
+  {
+    "revision": "8543c8417557456632a5",
+    "url": "/movieApp/static/js/main.8543c841.chunk.js"
+  },
+  {
+    "revision": "af0846f34c26a370a1d2",
+    "url": "/movieApp/static/js/1.af0846f3.chunk.js"
+  },
+  {
+    "revision": "8543c8417557456632a5",
+    "url": "/movieApp/static/css/main.ca054d4c.chunk.css"
+  },
+  {
+    "revision": "9f64be52470404e0bf21c9b79e86d1da",
+    "url": "/movieApp/index.html"
+  }
+];
